624477319c518282a5a07c358dcd43692402da7d
gaby-grafsgaard <gabyinhawaii@gmail.com>
2025-02-13 10:53:40 -1000

    Description of Pikakes behavior

--- src/com/Lei/PikakeLei.java
+++ src/com/Lei/PikakeLei.java
@@ -4,6 +4,8 @@ public class PikakeLei {
 
 	public static void main(String[] args) {
 System.out.println("Pikake lei is one of the most fragant leis");
+System.out.println("Pikake moves depending if its morning or night");
+System.out.println("Pikake is a vine");
 	}
 
 }
